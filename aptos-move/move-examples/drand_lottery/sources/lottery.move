aptos-labs/aptos-core

example of a drand-based lottery

aptos-move/move-examples/drand_lottery/sources/lottery.move
@@ -1,0 +1,172 @@
+module drand::lottery {
+    use std::signer;
+    use aptos_framework::account;
+    use std::vector;
+    use std::option::{Self, Option};
+    use aptos_framework::coin;
+    use std::error;
+    use aptos_framework::timestamp;
+    use aptos_framework::aptos_coin::AptosCoin;
+    use drand::drand;
+    use aptos_std::type_info;
+    use std::string::{Self};
+    //use aptos_std::debug;
+
+    /// Error code code when someone tries to start a very "short" lottery where users might not have enough time to buy tickets.
+    const E_LOTTERY_IS_NOT_LONG_ENOUGH: u64 = 0;
+    /// Error code for when someone tries to modify the time when the lottery is drawn. Once set, this time cannot be modified (for simplicity).
+    const E_LOTTERY_ALREADY_STARTED: u64 = 1;
+    /// Error code for when a user tries to purchase a ticket after the lottery has closed. This would not be secure since such users might know the public randomness, which is revealed soon after the lottery has closed.
+    const E_LOTTERY_HAS_CLOSED: u64 = 2;
+    /// Error code for when a user tries to initiating the drawing too early (enough time must've elapsed since the lottery started for users to have time to register).
+    const E_LOTTERY_DRAW_IS_TOO_EARLY: u64 = 3;
+    /// Error code for when anyone submits an incorrect randomness for the randomized draw phase of the lottery.
+    const E_INCORRECT_RANDOMNESS: u64 = 4;
+
+    /// The minimum time between when a lottery is 'started' and when it's closed & the randomized drawing can happen.
+    /// Currently set to (10 mins * 60 secs / min) seconds.
+    const MINIMUM_LOTTERY_DURATION_SECS: u64 = 10 * 60;
+
+    /// The minimum price of a lottery ticket.
+    const TICKET_PRICE: u64 = 10000;
+
+    /// A lottery: a list of users who bought tickets and a time past which the randomized drawing can happen.
+    ///
+    /// The winning user will be randomly picked (via drand public randomness) from this list.
+    struct Lottery has key {
+        // A list of which users bought lottery tickets
+        tickets: vector<address>,
+
+        // The time when the lottery ends (and thus when the drawing happens).
+        // Specifically, the drawing will happen during the drand round at time `draw_at`.
+        // `None` if the lottery is in the 'not started' state.
+        end_time: Option<u64>,
+
+        // Signer for the resource accounts storing the coins that can be won
+        signer_cap: account::SignerCapability,
+    }
+
+    // Declare the testing module as a friend, so it can call `init_module` below for testing.
+    friend drand::lottery_test;
+
+    /// Initializes a so-called "resource" account which will maintain the list of lottery tickets bought by users.
+    public(friend) fun init_module(deployer: &signer) {
+        // Create the resource account. This will allow this module to later obtain a `signer` for this account and
+        // update the list of purchased lottery tickets.
+        let (_resource, signer_cap) = account::create_resource_account(deployer, vector::empty());
+
+        // Acquire a signer for the resource account that stores the coin bounty
+        let rsrc_acc_signer = account::create_signer_with_capability(&signer_cap);
+
+        // Initialize an AptosCoin coin store there, which is where the lottery bounty will be kept
+        coin::register<AptosCoin>(&rsrc_acc_signer);
+
+        // Initialiaze the loterry as 'not started'
+        move_to(deployer,
+            Lottery {
+                tickets: vector::empty<address>(),
+                end_time: option::none(),
+                signer_cap,
+            }
+        )
+    }
+
+    public fun get_ticket_price(): u64 { TICKET_PRICE }
+    public fun get_minimum_lottery_duration_in_secs(): u64 { MINIMUM_LOTTERY_DURATION_SECS }
+
+    /// Allows anyone to start & configure the lottery so that drawing happens at time `draw_at` (and thus users
+    /// have plenty of time to buy tickets), where `draw_at` is a UNIX timestamp in seconds.
+    ///
+    /// NOTE: A real application can access control this.
+    public entry fun start_lottery(end_time_secs: u64) acquires Lottery {
+        // Make sure the lottery stays open long enough for people to buy tickets.
+        assert!(end_time_secs >= timestamp::now_seconds() + MINIMUM_LOTTERY_DURATION_SECS, error::out_of_range(E_LOTTERY_IS_NOT_LONG_ENOUGH));
+
+        // Update the Lottery resource with the (future) lottery drawing time, effectively 'starting' the lottery.
+        let lottery = borrow_global_mut<Lottery>(@drand);
+        assert!(option::is_none(&lottery.end_time), error::permission_denied(E_LOTTERY_ALREADY_STARTED));
+        lottery.end_time = option::some(end_time_secs);
+
+        //debug::print(&string::utf8(b"Started a lottery that will draw at time: "));
+        //debug::print(&draw_at_in_secs);
+    }
+
+    /// Called by any user to purchase a ticket in the lottery.
+    public entry fun buy_a_ticket(user: &signer) acquires Lottery {
+        // Get the Lottery resource
+        let lottery = borrow_global_mut<Lottery>(@drand);
+
+        // Make sure the lottery has been 'started' but has NOT been 'drawn' yet
+        let draw_at = *option::borrow(&lottery.end_time);
+        assert!(timestamp::now_seconds() < draw_at, error::out_of_range(E_LOTTERY_HAS_CLOSED));
+
+        // Get the address of the resource account that stores the coin bounty
+        let (_, rsrc_acc_addr) = get_rsrc_acc(lottery);
+
+        // Charge the price of a lottery ticket from the user's balance, and accumulate it into the lottery's bounty
+        coin::transfer<AptosCoin>(user, rsrc_acc_addr, TICKET_PRICE);
+
+        // ...and issue a ticket for that user
+        vector::push_back(&mut lottery.tickets, signer::address_of(user))
+    }
+
+    /// Allows anyone to close the lottery (if enough time has elapsed) and to decide the winner, by uploading
+    /// the correct _drand-signed bytes_ associated with the committed draw time in `Lottery::draw_post`.
+    /// These bytes will then be verified and used to extract randomness.
+    public entry fun close_lottery(drand_signed_bytes: vector<u8>): Option<address> acquires Lottery {
+        // Get the Lottery resource
+        let lottery = borrow_global_mut<Lottery>(@drand);
+
+        // Make sure the lottery has been 'started' and enough time has elapsed before the drawing can start
+        let draw_at = *option::borrow(&lottery.end_time);
+        assert!(timestamp::now_seconds() >= draw_at, error::out_of_range(E_LOTTERY_DRAW_IS_TOO_EARLY));
+
+        // It could be that no one signed up...
+        if(vector::is_empty(&lottery.tickets)) {
+            // It's time to draw, but nobody signed up => nobody won.
+            // Close the lottery (even if the randomness might be incorrect).
+            option::extract(&mut lottery.end_time);
+            return option::none<address>()
+        };
+
+        // Determine the next drand round after `draw_at`
+        let drand_round = drand::next_round_after(draw_at);
+
+        // Verify the randomness for this round and pick a winner
+        let randomness = drand::verify_and_extract_randomness(drand_signed_bytes, drand_round);
+        assert!(option::is_some(&randomness), error::permission_denied(E_INCORRECT_RANDOMNESS));
+
+        // Use the bytes to pick a number at random from 0 to `|lottery.tickets| - 1` and select the winner
+        let randomness = option::extract(&mut randomness);
+
+        let dst = type_info::type_name<Lottery>();
+        let dst = string::bytes(&dst);
+        let winner_idx = drand::uniform_random_less_than_n(*dst, randomness, vector::length(&lottery.tickets));
+
+        // Pay the winner
+        let (rsrc_acc_signer, rsrc_acc_addr) = get_rsrc_acc(lottery);
+        let balance = coin::balance<AptosCoin>(rsrc_acc_addr);
+        let winner_addr = *vector::borrow(&lottery.tickets, winner_idx);
+
+        coin::transfer<AptosCoin>(
+            &rsrc_acc_signer,
+            winner_addr,
+            balance);
+
+        // Close the lottery
+        option::extract(&mut lottery.end_time);
+        lottery.tickets = vector::empty<address>();
+        option::some(winner_addr)
+    }
+
+    //
+    // Internal functions
+    //
+
+    fun get_rsrc_acc(lottery: &Lottery): (signer, address) {
+        let rsrc_acc_signer = account::create_signer_with_capability(&lottery.signer_cap);
+        let rsrc_acc_addr = signer::address_of(&rsrc_acc_signer);
+
+        (rsrc_acc_signer, rsrc_acc_addr)
+    }
+}

aptos-move/move-examples/drand_lottery/sources/lottery_test.move
@@ -1,0 +1,125 @@
+#[test_only]
+module drand::lottery_test {
+    use drand::lottery;
+    use aptos_framework::timestamp;
+    use std::signer;
+    use aptos_framework::account;
+    use aptos_framework::coin;
+    use aptos_framework::aptos_coin::{Self, AptosCoin};
+    use std::option;
+    use aptos_std::debug;
+    use std::string;
+    use aptos_framework::coin::MintCapability;
+    use std::vector;
+
+    fun give_coins(mint_cap: &MintCapability<AptosCoin>, to: &signer) {
+        let to_addr = signer::address_of(to);
+        if (!account::exists_at(to_addr)) {
+            account::create_account_for_test(to_addr);
+        };
+        coin::register<AptosCoin>(to);
+
+        let coins = coin::mint(lottery::get_ticket_price(), mint_cap);
+        coin::deposit(to_addr, coins);
+    }
+
+    #[test(myself = @drand, fx = @aptos_framework, u1 = @0xA001, u2 = @0xA002, u3 = @0xA003, u4 = @0xA004)]
+    fun test_lottery(
+        myself: signer, fx: signer,
+        u1: signer, u2: signer, u3: signer, u4: signer,
+    ) {
+        timestamp::set_time_has_started_for_testing(&fx);
+
+        // Deploy the lottery smart contract
+        lottery::init_module(&myself);
+
+        // Needed to mint coins out of thin air for testing
+        let (burn_cap, mint_cap) = aptos_coin::initialize_for_test(&fx);
+
+        // We simulate different runs of the lottery to demonstrate the uniformity of the outcomes
+        let vec_signed_bytes = vector::empty<vector<u8>>();
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900000"); // u3 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900001"); // u3 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900002"); // u3 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900003"); // u1 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900004"); // u4 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900005"); // u4 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900006"); // u4 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd12345678900007"); // u2 wins
+
+        let lottery_start_time_secs = 1677685200; // the time that the 1st drand epoch started
+        let lottery_duration = lottery::get_minimum_lottery_duration_in_secs();
+
+        // We pop_back, so we reverse the vector to simulate pop_front
+        vector::reverse(&mut vec_signed_bytes);
+
+        while(!vector::is_empty(&vec_signed_bytes)) {
+            let signed_bytes = vector::pop_back(&mut vec_signed_bytes);
+
+            // Create fake coins for users participating in lottery & initialize aptos_framework
+            give_coins(&mint_cap, &u1);
+            give_coins(&mint_cap, &u2);
+            give_coins(&mint_cap, &u3);
+            give_coins(&mint_cap, &u4);
+
+            // Simulates the lottery starting at the current blockchain time
+            timestamp::update_global_time_for_test(lottery_start_time_secs * 1000 * 1000);
+
+            test_lottery_with_randomness(
+                &u1, &u2, &u3, &u4,
+                lottery_start_time_secs, lottery_duration,
+                signed_bytes
+            );
+
+            // Shift the next lottery's start time a little (otherwise, timestamp::update_global_time_for_test fails
+            // when resetting the time back to the past).
+            lottery_start_time_secs = lottery_start_time_secs + 2 * lottery_duration;
+        };
+
+        // Clean up
+        coin::destroy_burn_cap<AptosCoin>(burn_cap);
+        coin::destroy_mint_cap<AptosCoin>(mint_cap);
+    }
+
+    #[test_only]
+    fun test_lottery_with_randomness(
+        u1: &signer, u2: &signer, u3: &signer, u4: &signer,
+        lottery_start_time_secs: u64,
+        lottery_duration: u64,
+        drand_signed_bytes: vector<u8>,
+    ) {
+        //debug::print(&string::utf8(b"The lottery duration is: "));
+        //debug::print(&lottery_duration);
+        //debug::print(&string::utf8(b"The time before starting it is: "));
+        //debug::print(&timestamp::now_seconds());
+
+        let lottery_draw_at_time = lottery_start_time_secs + lottery_duration;
+
+        //
+        // Send a TXN to start the lottery
+        //
+        lottery::start_lottery(lottery_draw_at_time);
+
+        //
+        // Each user sends a TXN to buy their ticket
+        //
+        lottery::buy_a_ticket(u1);
+        lottery::buy_a_ticket(u2);
+        lottery::buy_a_ticket(u3);
+        lottery::buy_a_ticket(u4);
+
+        // Advance time far enough so the lottery can be closed
+        timestamp::fast_forward_seconds(lottery_duration);
+        assert!(timestamp::now_seconds() == lottery_draw_at_time, 1);
+        //debug::print(&string::utf8(b"The time before closing is: "));
+        //debug::print(&timestamp::now_seconds());
+
+        //
+        // Send a TXN with `drand_signed_bytes` to close the lottery and determine the winner
+        //
+        let winner_addr = option::extract(&mut lottery::close_lottery(drand_signed_bytes));
+
+        debug::print(&string::utf8(b"The winner is: "));
+        debug::print(&winner_addr)
+    }
+}

Ivan-Shish/Aptos34

aptos-move/move-examples/drand_lottery/sources/lottery.move
@@ -1,0 +1,169 @@
+module drand::lottery {
+    use std::signer;
+    use aptos_framework::account;
+    use std::vector;
+    use std::option::{Self, Option};
+    use aptos_framework::coin;
+    use std::error;
+    use aptos_framework::timestamp;
+    use aptos_framework::aptos_coin::AptosCoin;
+    use drand::drand;
+    use aptos_std::type_info;
+    use std::string::{Self};
+    //use aptos_std::debug;
+
+    /// Error code code when someone tries to start a very "short" lottery where users might not have enough time to buy tickets.
+    const E_LOTTERY_IS_NOT_LONG_ENOUGH: u64 = 0;
+    /// Error code for when someone tries to modify the time when the lottery is drawn. Once set, this time cannot be modified (for simplicity).
+    const E_LOTTERY_ALREADY_STARTED: u64 = 1;
+    /// Error code for when a user tries to purchase a ticket after the lottery has closed. This would not be secure since such users might know the public randomness, which is revealed soon after the lottery has closed.
+    const E_LOTTERY_HAS_CLOSED: u64 = 2;
+    /// Error code for when a user tries to initiating the drawing too early (enough time must've elapsed since the lottery started for users to have time to register).
+    const E_LOTTERY_DRAW_IS_TOO_EARLY: u64 = 3;
+    /// Error code for when anyone submits an incorrect randomness for the randomized draw phase of the lottery.
+    const E_INCORRECT_RANDOMNESS: u64 = 4;
+
+    /// The minimum time between when a lottery is 'started' and when it's closed & the randomized drawing can happen.
+    /// Currently set to (10 mins * 60 secs / min) seconds.
+    const MINIMUM_LOTTERY_DURATION_SECS: u64 = 10 * 60;
+
+    /// The minimum price of a lottery ticket.
+    const TICKET_PRICE: u64 = 10000;
+
+    /// A lottery: a list of users who bought tickets and a time past which the randomized drawing can happen.
+    ///
+    /// The winning user will be randomly picked (via drand public randomness) from this list.
+    struct Lottery has key {
+        // A list of which users bought lottery tickets
+        tickets: vector<address>,
+
+        // The time when the lottery ends (and thus when the drawing happens).
+        // Specifically, the drawing will happen during the next drand round after time `end_time`.
+        // `None` if the lottery is in the 'not started' state.
+        end_time: Option<u64>,
+
+        // Signer for the resource accounts storing the coins that can be won
+        signer_cap: account::SignerCapability,
+    }
+
+    // Declare the testing module as a friend, so it can call `init_module` below for testing.
+    friend drand::lottery_test;
+
+    /// Initializes a so-called "resource" account which will maintain the list of lottery tickets bought by users.
+    public(friend) fun init_module(deployer: &signer) {
+        // Create the resource account. This will allow this module to later obtain a `signer` for this account and
+        // update the list of purchased lottery tickets.
+        let (_resource, signer_cap) = account::create_resource_account(deployer, vector::empty());
+
+        // Acquire a signer for the resource account that stores the coin bounty
+        let rsrc_acc_signer = account::create_signer_with_capability(&signer_cap);
+
+        // Initialize an AptosCoin coin store there, which is where the lottery bounty will be kept
+        coin::register<AptosCoin>(&rsrc_acc_signer);
+
+        // Initialiaze the loterry as 'not started'
+        move_to(deployer,
+            Lottery {
+                tickets: vector::empty<address>(),
+                end_time: option::none(),
+                signer_cap,
+            }
+        )
+    }
+
+    public fun get_ticket_price(): u64 { TICKET_PRICE }
+    public fun get_minimum_lottery_duration_in_secs(): u64 { MINIMUM_LOTTERY_DURATION_SECS }
+
+    /// Allows anyone to start & configure the lottery so that drawing happens at time `end_time_secs` (and thus users
+    /// have plenty of time to buy tickets), where `end_time_secs` is a UNIX timestamp in seconds.
+    ///
+    /// NOTE: A real application can access control this.
+    public entry fun start_lottery(end_time_secs: u64) acquires Lottery {
+        // Make sure the lottery stays open long enough for people to buy tickets.
+        assert!(end_time_secs >= timestamp::now_seconds() + MINIMUM_LOTTERY_DURATION_SECS, error::out_of_range(E_LOTTERY_IS_NOT_LONG_ENOUGH));
+
+        // Update the Lottery resource with the (future) lottery drawing time, effectively 'starting' the lottery.
+        let lottery = borrow_global_mut<Lottery>(@drand);
+        assert!(option::is_none(&lottery.end_time), error::permission_denied(E_LOTTERY_ALREADY_STARTED));
+        lottery.end_time = option::some(end_time_secs);
+
+        //debug::print(&string::utf8(b"Started a lottery that will draw at time: "));
+        //debug::print(&end_time_secs);
+    }
+
+    /// Called by any user to purchase a ticket in the lottery.
+    public entry fun buy_a_ticket(user: &signer) acquires Lottery {
+        // Get the Lottery resource
+        let lottery = borrow_global_mut<Lottery>(@drand);
+
+        // Make sure the lottery has been 'started' but has NOT been 'drawn' yet
+        let end_time = *option::borrow(&lottery.end_time);
+        assert!(timestamp::now_seconds() < end_time, error::out_of_range(E_LOTTERY_HAS_CLOSED));
+
+        // Get the address of the resource account that stores the coin bounty
+        let (_, rsrc_acc_addr) = get_rsrc_acc(lottery);
+
+        // Charge the price of a lottery ticket from the user's balance, and accumulate it into the lottery's bounty
+        coin::transfer<AptosCoin>(user, rsrc_acc_addr, TICKET_PRICE);
+
+        // ...and issue a ticket for that user
+        vector::push_back(&mut lottery.tickets, signer::address_of(user))
+    }
+
+    /// Allows anyone to close the lottery (if enough time has elapsed) and to decide the winner, by uploading
+    /// the correct _drand-signed bytes_ associated with the committed draw time in `Lottery::draw_post`.
+    /// These bytes will then be verified and used to extract randomness.
+    public entry fun close_lottery(drand_signed_bytes: vector<u8>): Option<address> acquires Lottery {
+        // Get the Lottery resource
+        let lottery = borrow_global_mut<Lottery>(@drand);
+
+        // Make sure the lottery has been 'started' and enough time has elapsed before the drawing can start
+        let end_time = *option::borrow(&lottery.end_time);
+        assert!(timestamp::now_seconds() >= end_time, error::out_of_range(E_LOTTERY_DRAW_IS_TOO_EARLY));
+
+        // It could be that no one signed up...
+        if(vector::is_empty(&lottery.tickets)) {
+            // It's time to draw, but nobody signed up => nobody won.
+            // Close the lottery (even if the randomness might be incorrect).
+            option::extract(&mut lottery.end_time);
+            return option::none<address>()
+        };
+
+        // Verify the randomness for the next drand round after `end_time` and pick a winner
+        let randomness = drand::verify_and_extract_next_randomness(drand_signed_bytes, end_time);
+        assert!(option::is_some(&randomness), error::permission_denied(E_INCORRECT_RANDOMNESS));
+
+        // Use the bytes to pick a number at random from 0 to `|lottery.tickets| - 1` and select the winner
+        let randomness = option::extract(&mut randomness);
+
+        let dst = type_info::type_name<Lottery>();
+        let dst = string::bytes(&dst);
+        let winner_idx = drand::uniform_random_less_than_n(*dst, randomness, vector::length(&lottery.tickets));
+
+        // Pay the winner
+        let (rsrc_acc_signer, rsrc_acc_addr) = get_rsrc_acc(lottery);
+        let balance = coin::balance<AptosCoin>(rsrc_acc_addr);
+        let winner_addr = *vector::borrow(&lottery.tickets, winner_idx);
+
+        coin::transfer<AptosCoin>(
+            &rsrc_acc_signer,
+            winner_addr,
+            balance);
+
+        // Close the lottery
+        option::extract(&mut lottery.end_time);
+        lottery.tickets = vector::empty<address>();
+        option::some(winner_addr)
+    }
+
+    //
+    // Internal functions
+    //
+
+    fun get_rsrc_acc(lottery: &Lottery): (signer, address) {
+        let rsrc_acc_signer = account::create_signer_with_capability(&lottery.signer_cap);
+        let rsrc_acc_addr = signer::address_of(&rsrc_acc_signer);
+
+        (rsrc_acc_signer, rsrc_acc_addr)
+    }
+}

aptos-move/move-examples/drand_lottery/sources/lottery_test.move
@@ -1,0 +1,125 @@
+#[test_only]
+module drand::lottery_test {
+    use drand::lottery;
+    use aptos_framework::timestamp;
+    use std::signer;
+    use aptos_framework::account;
+    use aptos_framework::coin;
+    use aptos_framework::aptos_coin::{Self, AptosCoin};
+    use std::option;
+    use aptos_std::debug;
+    use std::string;
+    use aptos_framework::coin::MintCapability;
+    use std::vector;
+
+    fun give_coins(mint_cap: &MintCapability<AptosCoin>, to: &signer) {
+        let to_addr = signer::address_of(to);
+        if (!account::exists_at(to_addr)) {
+            account::create_account_for_test(to_addr);
+        };
+        coin::register<AptosCoin>(to);
+
+        let coins = coin::mint(lottery::get_ticket_price(), mint_cap);
+        coin::deposit(to_addr, coins);
+    }
+
+    #[test(myself = @drand, fx = @aptos_framework, u1 = @0xA001, u2 = @0xA002, u3 = @0xA003, u4 = @0xA004)]
+    fun test_lottery(
+        myself: signer, fx: signer,
+        u1: signer, u2: signer, u3: signer, u4: signer,
+    ) {
+        timestamp::set_time_has_started_for_testing(&fx);
+
+        // Deploy the lottery smart contract
+        lottery::init_module(&myself);
+
+        // Needed to mint coins out of thin air for testing
+        let (burn_cap, mint_cap) = aptos_coin::initialize_for_test(&fx);
+
+        // We simulate different runs of the lottery to demonstrate the uniformity of the outcomes
+        let vec_signed_bytes = vector::empty<vector<u8>>();
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789000"); // u1 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789001"); // u2 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789002"); // u3 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789003"); // u3 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789004"); // u4 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789005"); // u3 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789006"); // u3 wins
+        vector::push_back(&mut vec_signed_bytes, x"c0ffeedeadbeef1337acbd123456789007"); // u1 wins
+
+        let lottery_start_time_secs = 1677685200; // the time that the 1st drand epoch started
+        let lottery_duration = lottery::get_minimum_lottery_duration_in_secs();
+
+        // We pop_back, so we reverse the vector to simulate pop_front
+        vector::reverse(&mut vec_signed_bytes);
+
+        while(!vector::is_empty(&vec_signed_bytes)) {
+            let signed_bytes = vector::pop_back(&mut vec_signed_bytes);
+
+            // Create fake coins for users participating in lottery & initialize aptos_framework
+            give_coins(&mint_cap, &u1);
+            give_coins(&mint_cap, &u2);
+            give_coins(&mint_cap, &u3);
+            give_coins(&mint_cap, &u4);
+
+            // Simulates the lottery starting at the current blockchain time
+            timestamp::update_global_time_for_test(lottery_start_time_secs * 1000 * 1000);
+
+            test_lottery_with_randomness(
+                &u1, &u2, &u3, &u4,
+                lottery_start_time_secs, lottery_duration,
+                signed_bytes
+            );
+
+            // Shift the next lottery's start time a little (otherwise, timestamp::update_global_time_for_test fails
+            // when resetting the time back to the past).
+            lottery_start_time_secs = lottery_start_time_secs + 2 * lottery_duration;
+        };
+
+        // Clean up
+        coin::destroy_burn_cap<AptosCoin>(burn_cap);
+        coin::destroy_mint_cap<AptosCoin>(mint_cap);
+    }
+
+    #[test_only]
+    fun test_lottery_with_randomness(
+        u1: &signer, u2: &signer, u3: &signer, u4: &signer,
+        lottery_start_time_secs: u64,
+        lottery_duration: u64,
+        drand_signed_bytes: vector<u8>,
+    ) {
+        //debug::print(&string::utf8(b"The lottery duration is: "));
+        //debug::print(&lottery_duration);
+        //debug::print(&string::utf8(b"The time before starting it is: "));
+        //debug::print(&timestamp::now_seconds());
+
+        let lottery_draw_at_time = lottery_start_time_secs + lottery_duration;
+
+        //
+        // Send a TXN to start the lottery
+        //
+        lottery::start_lottery(lottery_draw_at_time);
+
+        //
+        // Each user sends a TXN to buy their ticket
+        //
+        lottery::buy_a_ticket(u1);
+        lottery::buy_a_ticket(u2);
+        lottery::buy_a_ticket(u3);
+        lottery::buy_a_ticket(u4);
+
+        // Advance time far enough so the lottery can be closed
+        timestamp::fast_forward_seconds(lottery_duration);
+        assert!(timestamp::now_seconds() == lottery_draw_at_time, 1);
+        //debug::print(&string::utf8(b"The time before closing is: "));
+        //debug::print(&timestamp::now_seconds());
+
+        //
+        // Send a TXN with `drand_signed_bytes` to close the lottery and determine the winner
+        //
+        let winner_addr = option::extract(&mut lottery::close_lottery(drand_signed_bytes));
+
+        debug::print(&string::utf8(b"The winner is: "));
+        debug::print(&winner_addr)
+    }
+}