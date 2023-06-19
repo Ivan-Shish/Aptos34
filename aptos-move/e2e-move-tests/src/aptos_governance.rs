Danialkaren/aptos-core

Support partial voting in aptos-governance

aptos-move/e2e-move-tests/src/aptos_governance.rs
@@ -1,0 +1,92 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::harness::MoveHarness;
+use aptos::move_tool::MemberId;
+use aptos_cached_packages::aptos_stdlib;
+use aptos_language_e2e_tests::account::Account;
+use aptos_types::{
+    account_address::AccountAddress, state_store::table::TableHandle,
+    transaction::TransactionStatus,
+};
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+#[derive(Deserialize, Serialize)]
+struct PartialVotingProposals {
+    pub proposals: TableHandle,
+}
+
+#[derive(Deserialize, Serialize)]
+struct RecordKey {
+    pub stake_pool: AccountAddress,
+    pub proposal_id: u64,
+}
+
+#[derive(Deserialize, Serialize)]
+struct VotingRecordsV2 {
+    pub votes: TableHandle,
+}
+
+pub fn create_proposal_v2(
+    harness: &mut MoveHarness,
+    account: &Account,
+    stake_pool: AccountAddress,
+    execution_hash: Vec<u8>,
+    metadata_location: Vec<u8>,
+    metadata_hash: Vec<u8>,
+    is_multi_step_proposal: bool,
+) -> TransactionStatus {
+    harness.run_transaction_payload(
+        account,
+        aptos_stdlib::aptos_governance_create_proposal_v2(
+            stake_pool,
+            execution_hash,
+            metadata_location,
+            metadata_hash,
+            is_multi_step_proposal,
+        ),
+    )
+}
+
+pub fn partial_vote(
+    harness: &mut MoveHarness,
+    account: &Account,
+    stake_pool: AccountAddress,
+    proposal_id: u64,
+    voting_power: u64,
+    should_pass: bool,
+) -> TransactionStatus {
+    harness.run_transaction_payload(
+        account,
+        aptos_stdlib::aptos_governance_partial_vote(
+            stake_pool,
+            proposal_id,
+            voting_power,
+            should_pass,
+        ),
+    )
+}
+
+pub fn is_partial_voting_proposal(harness: &mut MoveHarness, proposal_id: u64) -> bool {
+    let fun = MemberId::from_str("0x1::aptos_governance::is_partial_voting_proposal").unwrap();
+    let res = harness
+        .execute_view_function(fun, vec![], vec![bcs::to_bytes(&proposal_id).unwrap()])
+        .unwrap();
+    bcs::from_bytes::<bool>(&res[0]).unwrap()
+}
+
+pub fn get_voting_power_usage(
+    harness: &mut MoveHarness,
+    stake_pool: AccountAddress,
+    proposal_id: u64,
+) -> u64 {
+    let fun = MemberId::from_str("0x1::aptos_governance::get_voting_power_usage").unwrap();
+    let res = harness
+        .execute_view_function(fun, vec![], vec![
+            bcs::to_bytes(&stake_pool).unwrap(),
+            bcs::to_bytes(&proposal_id).unwrap(),
+        ])
+        .unwrap();
+    bcs::from_bytes::<u64>(&res[0]).unwrap()
+}

aptos-move/e2e-move-tests/src/tests/vote.rs
@@ -1,0 +1,153 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{
+    aptos_governance::*, assert_abort, assert_success, increase_lockup, setup_staking,
+    tests::common, MoveHarness,
+};
+use aptos_types::account_address::AccountAddress;
+use once_cell::sync::Lazy;
+use std::collections::BTreeMap;
+
+pub static PROPOSAL_SCRIPTS: Lazy<BTreeMap<String, Vec<u8>>> = Lazy::new(build_scripts);
+
+fn build_scripts() -> BTreeMap<String, Vec<u8>> {
+    let package_folder = "vote.data";
+    let package_names = vec!["enable_partial_governance_voting"];
+    common::build_scripts(package_folder, package_names)
+}
+
+#[test]
+fn test_vote() {
+    // Genesis starts with one validator with index 0
+    let mut harness = MoveHarness::new();
+    let validator_1 = harness.new_account_at(AccountAddress::from_hex_literal("0x123").unwrap());
+    let validator_2 = harness.new_account_at(AccountAddress::from_hex_literal("0x234").unwrap());
+    let validator_1_address = *validator_1.address();
+    let validator_2_address = *validator_2.address();
+
+    let stake_amount_1 = 25_000_000;
+    assert_success!(setup_staking(&mut harness, &validator_1, stake_amount_1));
+    assert_success!(increase_lockup(&mut harness, &validator_1));
+    let stake_amount_2 = 25_000_000;
+    assert_success!(setup_staking(&mut harness, &validator_2, stake_amount_2));
+    assert_success!(increase_lockup(&mut harness, &validator_2));
+
+    let mut proposal_id: u64 = 0;
+    assert_success!(create_proposal_v2(
+        &mut harness,
+        &validator_2,
+        validator_2_address,
+        vec![1],
+        vec![],
+        vec![],
+        true
+    ));
+    // Cannot create partial voting proposal before enable the feature flag.
+    assert!(!is_partial_voting_proposal(&mut harness, proposal_id));
+    // Voters can vote on a partial voting proposal but argument voting_power will be ignored.
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        100,
+        true
+    ));
+
+    // Enable partial governance voting. In production it requires governance.
+    let core_resources =
+        harness.new_account_at(AccountAddress::from_hex_literal("0xA550C18").unwrap());
+    let script_code = PROPOSAL_SCRIPTS
+        .get("enable_partial_governance_voting")
+        .expect("proposal script should be built");
+    let txn = harness.create_script(&core_resources, script_code.clone(), vec![], vec![]);
+    assert_success!(harness.run(txn));
+
+    // Voters cannot vote on a non partial voting proposal twice even after the feature flag is enabled.
+    assert_abort!(
+        partial_vote(
+            &mut harness,
+            &validator_1,
+            validator_1_address,
+            proposal_id,
+            100,
+            true
+        ),
+        0x10004
+    );
+
+    assert_success!(create_proposal_v2(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        vec![1],
+        vec![],
+        vec![],
+        true
+    ));
+
+    // Cannot vote on a non-exist proposal.
+    let wrong_proposal_id: u64 = 2;
+    assert_abort!(
+        partial_vote(
+            &mut harness,
+            &validator_1,
+            validator_1_address,
+            wrong_proposal_id,
+            100,
+            true
+        ),
+        25863
+    );
+
+    proposal_id = 1;
+    assert!(is_partial_voting_proposal(&mut harness, proposal_id));
+    assert_eq!(
+        get_voting_power_usage(&mut harness, validator_1_address, proposal_id),
+        0
+    );
+    assert_eq!(
+        get_voting_power_usage(&mut harness, validator_2_address, proposal_id),
+        0
+    );
+
+    // A voter can vote on a proposal multiple times with both Yes/No.
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        100,
+        true
+    ));
+    assert_eq!(
+        get_voting_power_usage(&mut harness, validator_1_address, proposal_id),
+        100
+    );
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        1000,
+        false
+    ));
+    assert_eq!(
+        get_voting_power_usage(&mut harness, validator_1_address, proposal_id),
+        1100
+    );
+    // A voter cannot use voting power more than it has.
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        stake_amount_1,
+        true
+    ));
+    assert_eq!(
+        get_voting_power_usage(&mut harness, validator_1_address, proposal_id),
+        stake_amount_1
+    );
+}

Ivan-Shish/Aptos34

aptos-move/e2e-move-tests/src/aptos_governance.rs
@@ -1,0 +1,84 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::harness::MoveHarness;
+use aptos::move_tool::MemberId;
+use aptos_cached_packages::aptos_stdlib;
+use aptos_language_e2e_tests::account::Account;
+use aptos_types::{
+    account_address::AccountAddress, state_store::table::TableHandle,
+    transaction::TransactionStatus,
+};
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+
+#[derive(Deserialize, Serialize)]
+struct PartialVotingProposals {
+    pub proposals: TableHandle,
+}
+
+#[derive(Deserialize, Serialize)]
+struct RecordKey {
+    pub stake_pool: AccountAddress,
+    pub proposal_id: u64,
+}
+
+#[derive(Deserialize, Serialize)]
+struct VotingRecordsV2 {
+    pub votes: TableHandle,
+}
+
+pub fn create_proposal_v2(
+    harness: &mut MoveHarness,
+    account: &Account,
+    stake_pool: AccountAddress,
+    execution_hash: Vec<u8>,
+    metadata_location: Vec<u8>,
+    metadata_hash: Vec<u8>,
+    is_multi_step_proposal: bool,
+) -> TransactionStatus {
+    harness.run_transaction_payload(
+        account,
+        aptos_stdlib::aptos_governance_create_proposal_v2(
+            stake_pool,
+            execution_hash,
+            metadata_location,
+            metadata_hash,
+            is_multi_step_proposal,
+        ),
+    )
+}
+
+pub fn partial_vote(
+    harness: &mut MoveHarness,
+    account: &Account,
+    stake_pool: AccountAddress,
+    proposal_id: u64,
+    voting_power: u64,
+    should_pass: bool,
+) -> TransactionStatus {
+    harness.run_transaction_payload(
+        account,
+        aptos_stdlib::aptos_governance_partial_vote(
+            stake_pool,
+            proposal_id,
+            voting_power,
+            should_pass,
+        ),
+    )
+}
+
+pub fn get_remaining_voting_power(
+    harness: &mut MoveHarness,
+    stake_pool: AccountAddress,
+    proposal_id: u64,
+) -> u64 {
+    let fun = MemberId::from_str("0x1::aptos_governance::get_remaining_voting_power").unwrap();
+    let res = harness
+        .execute_view_function(fun, vec![], vec![
+            bcs::to_bytes(&stake_pool).unwrap(),
+            bcs::to_bytes(&proposal_id).unwrap(),
+        ])
+        .unwrap();
+    bcs::from_bytes::<u64>(&res[0]).unwrap()
+}

aptos-move/e2e-move-tests/src/tests/vote.rs
@@ -1,0 +1,155 @@
+// Copyright © Aptos Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{
+    aptos_governance::*, assert_abort, assert_success, increase_lockup, setup_staking,
+    tests::common, MoveHarness,
+};
+use aptos_types::account_address::AccountAddress;
+use once_cell::sync::Lazy;
+use std::collections::BTreeMap;
+
+pub static PROPOSAL_SCRIPTS: Lazy<BTreeMap<String, Vec<u8>>> = Lazy::new(build_scripts);
+
+fn build_scripts() -> BTreeMap<String, Vec<u8>> {
+    let package_folder = "vote.data";
+    let package_names = vec!["enable_partial_governance_voting"];
+    common::build_scripts(package_folder, package_names)
+}
+
+#[test]
+fn test_vote() {
+    // Genesis starts with one validator with index 0
+    let mut harness = MoveHarness::new();
+    let validator_1 = harness.new_account_at(AccountAddress::from_hex_literal("0x123").unwrap());
+    let validator_2 = harness.new_account_at(AccountAddress::from_hex_literal("0x234").unwrap());
+    let validator_1_address = *validator_1.address();
+    let validator_2_address = *validator_2.address();
+
+    let stake_amount_1 = 25_000_000;
+    assert_success!(setup_staking(&mut harness, &validator_1, stake_amount_1));
+    assert_success!(increase_lockup(&mut harness, &validator_1));
+    let stake_amount_2 = 25_000_000;
+    assert_success!(setup_staking(&mut harness, &validator_2, stake_amount_2));
+    assert_success!(increase_lockup(&mut harness, &validator_2));
+
+    let mut proposal_id: u64 = 0;
+    assert_success!(create_proposal_v2(
+        &mut harness,
+        &validator_2,
+        validator_2_address,
+        vec![1],
+        vec![],
+        vec![],
+        true
+    ));
+    // Voters can vote on a partial voting proposal but argument voting_power will be ignored.
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        100,
+        true
+    ));
+    // No remaining voting power.
+    assert_eq!(
+        get_remaining_voting_power(&mut harness, validator_1_address, proposal_id),
+        0
+    );
+
+    // Enable partial governance voting. In production it requires governance.
+    let core_resources =
+        harness.new_account_at(AccountAddress::from_hex_literal("0xA550C18").unwrap());
+    let script_code = PROPOSAL_SCRIPTS
+        .get("enable_partial_governance_voting")
+        .expect("proposal script should be built");
+    let txn = harness.create_script(&core_resources, script_code.clone(), vec![], vec![]);
+    assert_success!(harness.run(txn));
+
+    // If a voter has already voted on a proposal before partial voting is enabled, the voter cannot vote on the proposal again.
+    assert_abort!(
+        partial_vote(
+            &mut harness,
+            &validator_1,
+            validator_1_address,
+            proposal_id,
+            100,
+            true
+        ),
+        0x10005
+    );
+
+    assert_success!(create_proposal_v2(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        vec![1],
+        vec![],
+        vec![],
+        true
+    ));
+
+    // Cannot vote on a non-exist proposal.
+    let wrong_proposal_id: u64 = 2;
+    assert_abort!(
+        partial_vote(
+            &mut harness,
+            &validator_1,
+            validator_1_address,
+            wrong_proposal_id,
+            100,
+            true
+        ),
+        25863
+    );
+
+    proposal_id = 1;
+    assert_eq!(
+        get_remaining_voting_power(&mut harness, validator_1_address, proposal_id),
+        stake_amount_1
+    );
+    assert_eq!(
+        get_remaining_voting_power(&mut harness, validator_2_address, proposal_id),
+        stake_amount_1
+    );
+
+    // A voter can vote on a proposal multiple times with both Yes/No.
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        100,
+        true
+    ));
+    assert_eq!(
+        get_remaining_voting_power(&mut harness, validator_1_address, proposal_id),
+        stake_amount_1 - 100
+    );
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        1000,
+        false
+    ));
+    assert_eq!(
+        get_remaining_voting_power(&mut harness, validator_1_address, proposal_id),
+        stake_amount_1 - 1100
+    );
+    // A voter cannot use voting power more than it has.
+    assert_success!(partial_vote(
+        &mut harness,
+        &validator_1,
+        validator_1_address,
+        proposal_id,
+        stake_amount_1,
+        true
+    ));
+    assert_eq!(
+        get_remaining_voting_power(&mut harness, validator_1_address, proposal_id),
+        0
+    );
+}